1b879c7a58cc9293fc0dc649d2d701aad2f3a1e6
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-23 14:54:39 +0530

    Adda generic class example

---
+++ ConsoleApp/Generics.cs
@@ -0,0 +1,22 @@
+﻿
+namespace GenericNameSpace
+{
+    internal class Message<T>
+    {
+        public Message(T message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    internal class Generics
+    {
+        public void Main(string[] args)
+        {
+            Message<string> message = new Message<string>("Hi");
+
+        }
+    }
+
+}
+

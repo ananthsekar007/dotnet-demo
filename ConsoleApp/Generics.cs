46a76a66bfc43d9da4b2e5c1baa7dfb866929d85
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-23 15:37:33 +0530

    Added stack implementation

--- ConsoleApp/Generics.cs
+++ ConsoleApp/Generics.cs
@@ -13,18 +13,29 @@ namespace GenericNameSpace
     {
         public static  void Main(string[] args)
         {
-            var exampleIntegerSet = new SortedSet<int>(){ 5, 8, 1, 3, 1, 3, 10 };
-            var exampleCharSet = new SortedSet<char>() { 'B', 'L', 'A', 'D' };
+            //var exampleIntegerSet = new SortedSet<int>(){ 5, 8, 1, 3, 1, 3, 10 };
+            //var exampleCharSet = new SortedSet<char>() { 'B', 'L', 'A', 'D' };
 
-            foreach(var example in exampleIntegerSet)
+            //foreach(var example in exampleIntegerSet)
+            //{
+            //    Console.WriteLine($" IntegerSet - {example}");
+            //}
+
+            //foreach (var example in exampleCharSet)
+            //{
+            //    Console.WriteLine($"Char set - {example}");
+            //}
+
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push("Hello");
+            stack.Push("World");
+
+            while(stack.Count > 0)
             {
-                Console.WriteLine($" IntegerSet - {example}");
+                Console.WriteLine(stack.Pop());
             }
 
-            foreach (var example in exampleCharSet)
-            {
-                Console.WriteLine($"Char set - {example}");
-            }
         }
     }
 

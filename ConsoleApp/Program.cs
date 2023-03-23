01cc292efad8e1bc83ce55c14aa694f3dc9d57ad
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-23 11:18:45 +0530

    Added Structure implementation  and Enum and array implementation

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,10 +4,49 @@ namespace ConsoleApp
 {
     internal class Program
     {
+        public struct Point
+        {
+             int xCoordinate;
+             int yCoordinate;
+
+            public void set(int xCoordinate, int yCoordinate)
+            {
+                this.xCoordinate = xCoordinate;
+                this.yCoordinate = yCoordinate;
+            }
+            public int diff()
+            {
+                return this.xCoordinate - this.yCoordinate;
+            }
+        }
+
+        public enum Weekdays
+        {
+            monday, tuesday, wednesday, thursday, friday
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("Next line");
+            string[] names = { "Ananth", "Sekar", "Nivetha" };
+            printArray(names);
+
+            Point point = new Point();
+
+            point.set(3, 2);
+
+            foreach(int weekday in Enum.GetValues(typeof(Weekdays))) {
+                Console.WriteLine(weekday);
+            }
+
+                   
         }
+
+        public static void printArray(string[] names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
     }
 }

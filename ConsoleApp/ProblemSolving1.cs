a6cce4897bffec5040fb9f02838ea7e9dd0affe3
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-24 15:49:41 +0530

    Added problem solving of string operations

---
+++ ConsoleApp/ProblemSolving1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class ProblemSolving1
+    {
+        public static void Main(string[] args)
+        {
+            StringOperation stringOperation = new(Console.ReadLine());
+            Console.WriteLine(stringOperation.ExchangeString());
+        }
+    }
+
+    internal class StringOperation
+    {
+        string firstString;
+        public StringOperation(string firstString)
+        {
+            this.firstString = firstString;
+        }
+
+        public string ExchangeString() {
+            return firstString.ElementAt(firstString.Length - 1) + firstString.Substring(1,firstString.Length - 2) + firstString.ElementAt(0);
+        }
+    }
+}

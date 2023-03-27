1cf0f5681617f80c27564b15b8f67a66b3274e3c
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-27 14:56:14 +0530

    Added different file methods

--- ConsoleApp/FileHandling.cs
+++ ConsoleApp/FileHandling.cs
@@ -10,16 +10,48 @@ namespace ConsoleApp
     {
         public static void Main(string[] args) {
             Console.WriteLine("File Handling!");
+
             const string path = "C:\\Users\\Hp\\Documents\\file.txt";
+            using (TextWriter textWriter = File.CreateText(path))
+            {
+                char[] charArray = { 'h', 'e', 'l', 'l', 'o' };
+                textWriter.WriteLine(charArray, 2, 3);
+            }
+
+            using (TextReader textReader =  File.OpenText(path))
+            {
+                Console.WriteLine(textReader.Read());
+            }
+
+            FileStream fileStream = new(path, FileMode.Open, FileAccess.Write);
             using (StreamWriter writetext = new StreamWriter(path))
             {
-                writetext.WriteLine("writing in text file");    
+                writetext.WriteLine("writing in text file");
+                writetext.Close();
+                fileStream.Close();
             }
 
             using (StreamReader readtext = new StreamReader(path))
             {
                 Console.WriteLine(readtext.ReadLine());
+                readtext.Close();
+                fileStream.Close();
+            }
+            using (StreamWriter writetext = new StreamWriter(path))
+            {
+                writetext.WriteLine("writing in text file");
+                writetext.Close();
+                fileStream.Close();
+            }
+
+            using (StreamReader readtext = new StreamReader(path))
+            {
+                Console.WriteLine(readtext.ReadLine());
+                readtext.Close();
+                fileStream.Close();
             }
         }
+
+        
     }
 }

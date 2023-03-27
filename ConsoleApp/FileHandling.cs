9d58ac20b27d4d2f1dd1d7823e3283af5210561e
ananthsekar007 <ananthsekar007@gmail.com>
2023-03-27 14:37:39 +0530

    Added a read operation using streamReader and writer

--- ConsoleApp/FileHandling.cs
+++ ConsoleApp/FileHandling.cs
@@ -10,9 +10,15 @@ namespace ConsoleApp
     {
         public static void Main(string[] args) {
             Console.WriteLine("File Handling!");
-            using (StreamWriter writetext = new StreamWriter("C:\\Users\\Hp\\Documents\\file.txt"))
+            const string path = "C:\\Users\\Hp\\Documents\\file.txt";
+            using (StreamWriter writetext = new StreamWriter(path))
             {
-                writetext.WriteLine("writing in text file");
+                writetext.WriteLine("writing in text file");    
+            }
+
+            using (StreamReader readtext = new StreamReader(path))
+            {
+                Console.WriteLine(readtext.ReadLine());
             }
         }
     }
